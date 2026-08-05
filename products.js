075d6e05c34cf1fc137f4be9c76b5041fbf01768
iwessgpt <iwess.gpt@gmail.com>
2026-08-06 04:37:19 +0500

    Update products.js

--- products.js
+++ products.js
@@ -4,5 +4,5 @@ const STORE_CONFIG = {
     storeDescription: "Premium Gadgets and Accessories",
     
     // This URL will be replaced with your actual published Google Sheet CSV link
-    sheetCsvUrl: "YOUR_GOOGLE_SHEET_CSV_URL_HERE"
+    sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSzPJaM9Z-umxRiA0akvyzqy45SXIpPwzuGHPeEjGONdtvSa476nuZ3A_xnkAjodPXNzUI9x3t27UuE/pub?output=csv"
 };
