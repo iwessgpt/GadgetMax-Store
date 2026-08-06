da7bd1a31f8fb99291d5d9f9994fd8aa9b19e340
iwessgpt <iwess.gpt@gmail.com>
2026-08-06 07:12:01 +0500

    Add files via upload

--- products.js
+++ products.js
@@ -4,5 +4,5 @@ const STORE_CONFIG = {
     storeDescription: "Premium Gadgets and Accessories",
     
     // This URL will be replaced with your actual published Google Sheet CSV link
-    sheetCsvUrl: "YOUR_GOOGLE_SHEET_CSV_URL_HERE"
+    sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIO7DF67iliHCdNEkSPOa6CoRphDiLAAStuxC4DYL3vNPwH_jhg4Mchnir2-JyJRmNcNmhQQU8wA-4/pub?output=csv"
 };
